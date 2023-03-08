bd00ef17bc747ff9924590d32c33b6c598c872cf
EmailKolar <Emilaalund.nyboder@gmail.com>
2023-03-08 09:33:43 +0100

    hello

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello gitWorld");
     }
+    void run(){
+
+    }
+
+
+    public void menu(){
+
+    }
+    public void printMenu(){
+
+    }
+
+
 }
---
+++ src/SQLHandler.java
@@ -0,0 +1,2 @@
+public class SQLHandler {
+}
